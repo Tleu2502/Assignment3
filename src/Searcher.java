5964c5cb0be87c71c830b92b552406adcd1e1b82
Admin <251331@astanait.edu.kz>
2026-04-27 19:30:02 +0500

    week4

--- src/Searcher.java
+++ src/Searcher.java
@@ -5,8 +5,11 @@ public class Searcher {
         int right = arr.length - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (arr[mid] == target) return mid;
-            else if (arr[mid] < target) left = mid + 1;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            }
             else right = mid - 1;
         }
         return -1;
